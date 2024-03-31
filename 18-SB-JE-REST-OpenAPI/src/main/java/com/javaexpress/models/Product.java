9ffa33e17e58fa0dd9ba1f2da29c75b5b9830220
ABHINAV GUMPU <105973197+AbhiJay1287@users.noreply.github.com>
2024-03-31 12:42:38 +0530

    Add files via upload

---
+++ 18-SB-JE-REST-OpenAPI/src/main/java/com/javaexpress/models/Product.java
@@ -0,0 +1,50 @@
+package com.javaexpress.models;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private Double price;
+	
+	private boolean active;
+	
+	private String description;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@CreationTimestamp
+	private Date createdTime;
+	
+	@UpdateTimestamp
+	private Date updatedTime;
+	
+	@ManyToOne
+	@JoinColumn(name="category_id",nullable = false)
+	private Category category;
+}
