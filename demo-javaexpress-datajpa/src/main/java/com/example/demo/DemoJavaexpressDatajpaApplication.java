1525a34935fc9ba7266bd8ce3e7c923ae6071cbb
ABHINAV GUMPU <105973197+AbhiJay1287@users.noreply.github.com>
2024-03-31 12:48:08 +0530

    Add files via upload

---
+++ demo-javaexpress-datajpa/src/main/java/com/example/demo/DemoJavaexpressDatajpaApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaexpressDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaexpressDatajpaApplication.class, args);
+		
+//		Category cat = new Category();
+		 
+		
+	}
+
+}
---
+++ lombok-integrationn/src/main/java/com/javaexpress/Test.java
@@ -0,0 +1,11 @@
+package com.javaexpress;
+
+public class Test {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setLastName("java");
+		emp.setLastName("Express");
+		
+		System.out.println(emp);
+	}
+}
